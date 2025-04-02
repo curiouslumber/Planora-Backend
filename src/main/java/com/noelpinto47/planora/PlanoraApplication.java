fbf91adc3c43bf1cdbca223378a27490fe21ecb1
Noel Pinto <noelpinto47@gmail.com>
2025-04-02 21:05:57 +0530

    first commit

---
+++ src/main/java/com/noelpinto47/planora/PlanoraApplication.java
@@ -0,0 +1,13 @@
+package com.noelpinto47.planora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noelpinto47/planora/PlanoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelpinto47.planora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
