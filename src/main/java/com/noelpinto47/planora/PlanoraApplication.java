8ff548b26c2048ca2045987303d791ca57ea0d7c
Noel Pinto <noelpinto47@gmail.com>
2025-05-08 16:04:39 +0530

    updated to start mysql database

--- src/main/java/com/noelpinto47/planora/PlanoraApplication.java
+++ src/main/java/com/noelpinto47/planora/PlanoraApplication.java
@@ -1,7 +1,13 @@
 package com.noelpinto47.planora;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PlanoraApplication {
@@ -10,4 +16,14 @@ public class PlanoraApplication {
 		SpringApplication.run(PlanoraApplication.class, args);
 	}
 
+	@Bean
+	public DataSource dataSource() {
+		Dotenv dotenv = Dotenv.load();
+		return DataSourceBuilder.create()
+				.driverClassName("com.mysql.cj.jdbc.Driver")
+				.url(dotenv.get("MYSQL_URL"))
+				.username(dotenv.get("MYSQL_USERNAME"))
+				.password(dotenv.get("MYSQL_PASSWORD"))
+				.build();
+	}
 }
