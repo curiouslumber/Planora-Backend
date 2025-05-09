f54b8d5e8016bf623177c362e2a8310d0c7b8b4c
Noel Pinto <noelpinto47@gmail.com>
2025-05-09 21:56:05 +0530

    fixed mysql on spring boot project

--- src/main/java/com/noelpinto47/planora/PlanoraConfiguration.java
+++ src/main/java/com/noelpinto47/planora/PlanoraConfiguration.java
@@ -4,27 +4,31 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Configuration
-@ConfigurationProperties(prefix = "database")
+@Component
+@ConfigurationProperties(prefix = "spring")
+@Data
 public class PlanoraConfiguration {
 
-    private String url;
-    private String username;
-    private String password;
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Data
+    public static class DataSourceProperties {
+        private String url;
+        private String username;
+        private String password;
 
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url(url)
-                .username(username)
-                .password(password)
-                .build();
+        @Bean
+        public DataSource dataSource() {
+            return DataSourceBuilder.create()
+                    .driverClassName("com.mysql.cj.jdbc.Driver")
+                    .url(url)
+                    .username(username)
+                    .password(password)
+                    .build();
+        }
     }
 }
